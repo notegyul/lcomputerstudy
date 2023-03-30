25c7f7a87722a5d45e5b9533eee3ad087948ab9f
notegyul <104202618+notegyul@users.noreply.github.com>
2023-03-30 09:33:25 +0900

    update user-detail

--- src/main/java/com/lcomputer/mymvc/database/DB_Connection.java
+++ src/main/java/com/lcomputer/mymvc/database/DB_Connection.java
@@ -9,14 +9,14 @@ public class DB_Connection {
 	public static Connection getConnection() throws SQLException, ClassNotFoundException {
 		Connection conn = null;
 		
-		String url = "jdbc:mysql://localhost:3306/mytest";
-		//String url = "jdbc:mariadb://localhost:3306/mytest";
+		//String url = "jdbc:mysql://localhost:3306/mytest";
+		String url = "jdbc:mariadb://localhost:3306/mytest";
 		String id = "root";
-		//String pw = "1234";
-		String pw = "ckatkflekd!99";
+		String pw = "1234";
+		//String pw = "ckatkflekd!99";
 		
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		//Class.forName("org.mariadb.jdbc.Driver");
+		//Class.forName("com.mysql.cj.jdbc.Driver");
+		Class.forName("org.mariadb.jdbc.Driver");
 		conn = DriverManager.getConnection(url,id,pw);
 		
 		return conn;
